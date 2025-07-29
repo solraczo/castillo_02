67f4032955b3552a5544cb48170a6a6cda195cbb
solraczo <79017916+solraczo@users.noreply.github.com>
2025-07-28 20:16:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function createColliderBox() {
 function loadAssets() {
     const loader = new THREE.GLTFLoader();
 
-    loader.load('models/castillo_02_11.gltf', (gltf) => {
+    loader.load('models/castillo_02_12.gltf', (gltf) => {
         const laberintoModel = gltf.scene;
         laberintoModel.scale.set(2, 2, 2);
         scene.add(laberintoModel);
